52618cb6b1664e9d9e7e194fb6b22919d0504549
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2025-05-08 23:42:46 +0700

    test component

--- fontend-react-mindmap/src/App.js
+++ fontend-react-mindmap/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
+import MyComponent from './MyComponent';
+
 const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
+  // const count = useSelector(state => state.counter.count);
+  // const dispatch = useDispatch();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>Count = {count}</div>
-        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      </header>
+    <div>
+      Heloo world
+      <MyComponent>
+
+      </MyComponent>
     </div>
   );
 }
 
 export default App;
+
+// <div className="App">
+//   <header className="App-header">
+//     <img src={logo} className="App-logo" alt="logo" />
+//     <p>
+//       Edit <code>src/App.js</code> and save to reload.
+//     </p>
+//     <div>Count = {count}</div>
+//     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//   </header>
+// </div> 
---
+++ fontend-react-mindmap/src/MyComponent.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    // JXS
+    render() {
+        return (
+            <div>
+                my first component
+                {Math.random()}
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
