43bd4001b16a4ad48ff1e2a1a933e7ba20c3eb39
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2025-06-26 15:15:18 +0700

    design practice, review flascard

--- 00-fontend-react_mindmap/src/Routes.jsx
+++ 00-fontend-react_mindmap/src/Routes.jsx
@@ -7,6 +7,9 @@ import Admin from './pages/Admin/Admin';
 import Dashboard from './pages/Admin/Dashboard';
 import FlashcardApp from './pages/Features/Flascard/Index';
 import CreateFlascard from './pages/Features/Flascard/CreateFlascard';
+import StudyFlascard from './pages/Features/Flascard/StudyFlascard';
+import PracticeTest from './pages/Features/Flascard/PracticeTest';
+import ReviewFlascard from './pages/Features/Flascard/ReviewFlascard';
 
 const AppRoutes = () => {
   return (
@@ -20,6 +23,9 @@ const AppRoutes = () => {
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="flashcards" element={<FlashcardApp />} />
           <Route path="flashcards/create" element={<CreateFlascard />} />
+          <Route path="flashcards/study" element={<StudyFlascard />} />
+          <Route path="flashcards/practice" element={<PracticeTest />} />
+          <Route path="flashcards/review" element={<ReviewFlascard />} />
         </Route>
 
       </Routes>
